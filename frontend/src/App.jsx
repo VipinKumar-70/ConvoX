6af8d507a55cc1758134d81583eedcac0c6f4416
VipinKumar-70 <vk20140074250@gmail.com>
2026-07-30 12:35:51 +0530

    implement real-time messaging with Socket.IO

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { api } from "./api/api";
+import socket from "./socket/socket";
 
 function App() {
-  const [form, setform] = useState();
-  const [message, setmessage] = useState();
+  const [inputMessage, setInputMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState([]);
 
+  // Test API connection
   useEffect(() => {
     async function testConnection() {
       try {
@@ -14,26 +16,56 @@ function App() {
         console.error(error);
       }
     }
+
     testConnection();
   }, []);
 
+  // Socket connection and message listener
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected:", socket.id);
+    });
+
+    socket.on("message", (message) => {
+      setChatMessages((previousMessages) => [...previousMessages, message]);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("message");
+    };
+  }, []);
+
+  function sendMessage() {
+    socket.emit("message", inputMessage);
+
+    setInputMessage("");
+  }
+
   return (
-    <>
-      <section id="center" className="p-5">
-        <h1>Testing Connection....</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter your message"
-            className="message-box border-2 p-2"
-          />
-          <button className="message-sent-btn p-2 bg-blue-500 mx-2 border-2">
-            Submit
-          </button>
-        </div>
-        <div className="message-container"></div>
-      </section>
-    </>
+    <section className="p-5">
+      <h1>Testing Connection....</h1>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your message"
+          className="border-2 p-2"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+        />
+
+        <button className="p-2 bg-blue-500 mx-2 border-2" onClick={sendMessage}>
+          Submit
+        </button>
+      </div>
+
+      <div className="message-container">
+        {chatMessages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
+    </section>
   );
 }
 
