e0d15ff7acb74b5509f78d1cafbc926bdb7469f6
VipinKumar-70 <vk20140074250@gmail.com>
2026-07-31 18:45:32 +0530

    feat: set up database and user schema

---
+++ backend/Models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, "username is required"],
+    unique: true,
+    lowecase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    minlength: 6,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
