990e05ca94bc4951bd56d6487431421a33e93969
VipinKumar-70 <vk20140074250@gmail.com>
2026-08-02 18:11:18 +0530

    feat: implement user authentication

--- backend/Controllers/userAuth.js
+++ backend/Controllers/userAuth.js
@@ -4,7 +4,7 @@ const brcypt = require("bcrypt");
 
 const register = async (req, res) => {
   try {
-    const { username, email, password } = body.req;
+    const { username, email, password } = req.body;
     const saltRound = 10;
     const myPlainTextPassword = password;
 
@@ -15,28 +15,68 @@ const register = async (req, res) => {
         .json({ success: false, message: "User already exits." });
     }
 
-    bcrypt.genSalt(saltRounds, function (err, salt) {
-      bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+    bcrypt.genSalt(saltRounds, async function (err, salt) {
+      bcrypt.hash(myPlaintextPassword, salt, async function (err, hash) {
         const createUser = await userModel.create({
-            username,
-            email,
-            password:hash,
-        })
+          username,
+          email,
+          password: hash,
+        });
         console.log(createUser);
-        
       });
       res.json({
-        success:true,
-        message:"User registered successfully."
-      })
+        success: true,
+        message: "User registered successfully.",
+      });
     });
   } catch (error) {
     console.log(error);
   }
 };
 
-const login = async () => {};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-const logout = async () => {};
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email and password" });
+    }
+
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (result) {
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+        res.cookie("userToken", token, {
+          httpOnly: true,
+          secure: false, // while deploying change its to true
+          sameSite: "lax",
+        });
+        return res.status(200).json({
+          success: true,
+          message: "Login successfully.",
+          token,
+          user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+          },
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const logout = async (req, res) => {
+  try {
+    res.clearCookie("userToken");
+    res.json({ message: "Logged out successfully." });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = { register, login, logout };
