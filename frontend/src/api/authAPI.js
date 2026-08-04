ad74411cdeb423e6b85ad0495a3959bc971aedc4
VipinKumar-70 <vk20140074250@gmail.com>
2026-08-04 22:47:14 +0530

    feat: add auth API integration

--- frontend/src/api/authAPI.js
+++ frontend/src/api/authAPI.js
@@ -1,1 +1,21 @@
 import api from "./api";
+
+export const login = async (credentials) => {
+  return api("/login", {
+    meethod: "POST",
+    body: JSON.stringify(credentials),
+  });
+};
+
+export const register = async (userData) => {
+  return api("/register", {
+    meethod: "POST",
+    body: JSON.stringify(userData),
+  });
+};
+
+export const logout = async () => {
+  return api("/logout", {
+    meethod: "POST",
+  });
+};
