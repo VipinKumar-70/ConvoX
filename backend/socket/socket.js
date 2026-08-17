8a56b3bc58eeca394331ddabedc4bb2442067be8
VipinKumar-70 <vk20140074250@gmail.com>
2026-08-17 18:38:26 +0530

    refactor: clean up socket connection logging

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -2,12 +2,18 @@ const socketConnection = (io) => {
   console.log("Socket.IO setup loaded");
 
   io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+    console.log("User connected with Socket ID:", socket.id);
 
     socket.on("message", (message) => {
-      console.log("Message received:", message);
+      console.log("Received message:", message);
 
       io.emit("message", message);
+    });
+
+    socket.on("disconnect", (reason) => {
+      console.log(
+        `User disconnected. Socket ID: ${socket.id}, Reason: ${reason}`,
+      );
     });
   });
 };
